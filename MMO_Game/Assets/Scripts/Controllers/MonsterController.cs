82a805407a5dfb867437ad0dd4c715d5ba06b03a
YongHyeon1231 <120100530+YongHyeon1231@users.noreply.github.com>
2024-02-09 02:30:22 +0900

    2024-02-09 객체끼리 충돌하면 이동하지 못하게 하고 이동애니메이션은 나오게

---
+++ MMO_Game/Assets/Scripts/Controllers/MonsterController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class MonsterController : CreatureController
+{
+    protected override void Init()
+    {
+        base.Init();
+
+        State = CreatureState.Idle;
+        Dir = MoveDir.None;
+    }
+
+    protected override void UpdateController()
+    {
+        base.UpdateController();
+    }
+}
