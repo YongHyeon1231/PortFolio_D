0d41cb91a7bc3b740244ae8199d07f997a532b86
YongHyeon1231 <120100530+YongHyeon1231@users.noreply.github.com>
2024-02-23 16:37:06 +0900

    2024-02-23 이동 동기화를 만족할 만한 수준으로 고쳤습니다.
    밑으로 쭉내려가다가 오른쪽으로 꺾었을때 장애물이 있다면 위치가 변하지 않고 상태 자체도 moving으로 변하지 않았습니다.
    그리고 처음에 Player가 뜀띠기 현상을 하고 있는데 고쳐주도록 하겠습니다.
    이걸 알아보기 쉬운 방법은 PacketHandler에서 S_EnterGameHandler에서 처음 플레이어의 정보를 패킷으로 전송받아서 추가할때이니까 여기다가 브레이크 포인트를 잡은다음에 확인해주면 됩니다.
    등등의 방식으로 해결했습니다.

--- MMO_Game/Client/Assets/Scripts/Controllers/CreatureController.cs
+++ MMO_Game/Client/Assets/Scripts/Controllers/CreatureController.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf.Protocol;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,40 +11,70 @@ public class CreatureController : MonoBehaviour
     [SerializeField]
     public float _speed = 5.0f;
 
-    public Vector3Int CellPos { get; set; } = Vector3Int.zero;
+    protected bool _updated = false;
+
+    PositionInfo _positionInfo = new PositionInfo();
+    public PositionInfo PosInfo
+    {
+        get { return _positionInfo; }
+        set
+        {
+            if (_positionInfo.Equals(value))
+                return;
+
+            CellPos = new Vector3Int(value.PosX, value.PosY, 0);
+            State = value.State;
+            Dir = value.MoveDir;
+        }
+    }
+
+    public Vector3Int CellPos
+    {
+        get
+        {
+            return new Vector3Int(PosInfo.PosX, PosInfo.PosY, 0);
+        }
+        set
+        {
+            if (PosInfo.PosX == value.x && PosInfo.PosY == value.y)
+                return;
+
+            PosInfo.PosX = value.x;
+            PosInfo.PosY = value.y;
+            _updated = true;
+        }
+    }
     protected Animator _animator;
     protected SpriteRenderer _sprite;
 
-    [SerializeField]
-    protected CreatureState _state = CreatureState.Idle;
     public virtual CreatureState State
     {
-        get { return _state; }
+        get { return PosInfo.State; }
         set
         {
-            if (_state == value) return;
+            if (PosInfo.State == value) return;
 
-            _state = value;
+            PosInfo.State = value;
             UpdateAnimation();
+            _updated = true;
         }
     }
 
     protected MoveDir _lastDir = MoveDir.Down;
-    [SerializeField]
-    protected MoveDir _dir = MoveDir.Down;
     public MoveDir Dir
     {
-        get { return _dir; }
+        get { return PosInfo.MoveDir; }
         set
         {
-            if (_dir == value)
+            if (PosInfo.MoveDir == value)
                 return;
 
-            _dir = value;
+            PosInfo.MoveDir = value;
             if (value != MoveDir.None)
                 _lastDir = value;
 
             UpdateAnimation();
+            _updated = true;
         }
     }
 
@@ -86,7 +117,7 @@ public class CreatureController : MonoBehaviour
 
     protected virtual void UpdateAnimation()
     {
-        if (_state == CreatureState.Idle)
+        if (State == CreatureState.Idle)
         {
             switch (_lastDir)
             {
@@ -108,9 +139,9 @@ public class CreatureController : MonoBehaviour
                     break;
             }
         }
-        else if (_state == CreatureState.Moving)
+        else if (State == CreatureState.Moving)
         {
-            switch (_dir)
+            switch (Dir)
             {
                 case MoveDir.Up:
                     _animator.Play("WALK_BACK");
@@ -132,7 +163,7 @@ public class CreatureController : MonoBehaviour
                     break;
             }
         }
-        else if (_state == CreatureState.Skill)
+        else if (State == CreatureState.Skill)
         {
             switch (_lastDir)
             {
@@ -178,6 +209,11 @@ public class CreatureController : MonoBehaviour
         _sprite = GetComponent<SpriteRenderer>();
         Vector3 pos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, 0.5f, 0);
         transform.position = pos;
+
+        State = CreatureState.Idle;
+        Dir = MoveDir.None;
+        CellPos = new Vector3Int(0, 0, 0);
+        UpdateAnimation();
     }
 
     protected virtual void UpdateController()
@@ -228,41 +264,7 @@ public class CreatureController : MonoBehaviour
 
     protected virtual void MoveToNextPos()
     {
-        // 키보드에 손을 떼었는지 확인
-        if (_dir == MoveDir.None)
-        {
-            State = CreatureState.Idle;
-            return;
-        }
-
-        // 키보드에 손이 계속 있다면 여기 반복
-        Vector3Int destPos = CellPos;
-
-        switch (_dir)
-        {
-            case MoveDir.Up:
-                destPos += Vector3Int.up;
-                break;
-            case MoveDir.Down:
-                destPos += Vector3Int.down;
-                break;
-            case MoveDir.Left:
-                destPos += Vector3Int.left;
-                break;
-            case MoveDir.Right:
-                destPos += Vector3Int.right;
-                break;
-            default:
-                break;
-        }
-
-        if (Managers.Map.CanGo(destPos))
-        {
-            if (Managers.Object.Find(destPos) == null)
-            {
-                CellPos = destPos;
-            }
-        }
+        
     }
 
     protected virtual void UpdateSkill()
--- MMO_Game/Client/Assets/Scripts/Controllers/MyPlayerController.cs
+++ MMO_Game/Client/Assets/Scripts/Controllers/MyPlayerController.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf.Protocol;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -71,6 +72,59 @@ public class MyPlayerController : PlayerController
         else
         {
             Dir = MoveDir.None;
+        }
+    }
+
+    protected override void MoveToNextPos()
+    {
+        // 키보드에 손을 떼었는지 확인
+        if (Dir == MoveDir.None)
+        {
+            State = CreatureState.Idle;
+            CheckUpdatedFlag();
+            return;
+        }
+
+        // 키보드에 손이 계속 있다면 여기 반복
+        Vector3Int destPos = CellPos;
+
+        switch (Dir)
+        {
+            case MoveDir.Up:
+                destPos += Vector3Int.up;
+                break;
+            case MoveDir.Down:
+                destPos += Vector3Int.down;
+                break;
+            case MoveDir.Left:
+                destPos += Vector3Int.left;
+                break;
+            case MoveDir.Right:
+                destPos += Vector3Int.right;
+                break;
+            default:
+                break;
+        }
+
+        if (Managers.Map.CanGo(destPos))
+        {
+            if (Managers.Object.Find(destPos) == null)
+            {
+                CellPos = destPos;
+            }
+        }
+
+        CheckUpdatedFlag();
+    }
+
+    void CheckUpdatedFlag()
+    {
+        if (_updated)
+        {
+            C_Move movePacket = new C_Move();
+            movePacket.PosInfo = PosInfo;
+            Managers.Network.Send(movePacket);
+            _updated = false;
         }
     }
 }
